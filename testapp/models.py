7bf7a95471dc684b00f08f968b2ec2cc7d1e681f
sivaram <sivaramk1411@gmail.com>
2021-05-26 08:55:39 +0530

    change location varable length 64 to 65

--- testapp/models.py
+++ testapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class Company(models.Model):
     name=models.CharField(max_length=128)
-    location=models.CharField(max_length=64)
+    location=models.CharField(max_length=65)
     ceo=models.CharField(max_length=65)
 
